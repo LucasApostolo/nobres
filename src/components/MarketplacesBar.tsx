b269e2244e3f43ca1b74c8a102be3fcf6356d21b
LucasApostolo <127116780+LucasApostolo@users.noreply.github.com>
2026-08-24 17:06:45 -0300

    Update MarketplaceLogo styles and structure

--- src/components/MarketplacesBar.tsx
+++ src/components/MarketplacesBar.tsx
@@ -6,21 +6,21 @@ const MarketplaceLogo = ({ id }: { id: string }) => {
   switch (id) {
     case 'mercadolivre':
       return (
-        <div className="flex items-center justify-center h-full w-full">
+        <div className="flex items-center justify-center w-full overflow-visible">
           <img 
             src="/logos/mercadolivre.png" 
             alt="Mercado Livre" 
-            className="h-14 w-auto object-contain scale-110" 
+            className="h-16 sm:h-20 w-auto object-contain scale-[2.2] filter brightness-200 contrast-125" 
           />
         </div>
       );
     case 'shopee':
       return (
-        <div className="flex items-center justify-center h-full w-full">
+        <div className="flex items-center justify-center w-full overflow-visible">
           <img 
             src="/logos/shopee.png" 
             alt="Shopee" 
-            className="h-12 w-auto object-contain scale-110" 
+            className="h-14 sm:h-16 w-auto object-contain scale-[2.1]" 
           />
         </div>
       );
@@ -44,7 +44,7 @@ const MarketplaceLogo = ({ id }: { id: string }) => {
     case 'shopify':
       return (
         <div className="flex items-center justify-center gap-2">
-          {/* Logo Oficial Shopify (Mantida Intacta) */}
+          {/* Logo Oficial Shopify (Intacto) */}
           <svg className="h-8 w-auto fill-current text-[#95BF47]" viewBox="0 0 108 122">
             <path d="M85.5 17.6c-1.2-3.8-4.2-4.1-4.2-4.1s-3.2-.3-6.2.7c-2.8-5.3-7.8-8.9-14.1-8.9h-.6c-1.3-2.1-3.6-3.5-6.2-3.5-4.4 0-7.8 3.4-8 7.7-4.5.9-8.5 3.3-11.2 6.8-4.1 5.3-4.8 12.6-4.8 12.6l-13.6 3.8s-4.3 1.2-5.4 5.3c-1 4.1.3 22.8 1.8 33.2 1.5 10.4 12.3 49.3 13.5 53.3 1.2 4 4.5 6.7 8.5 6.7 0 0 34.6 0 45.4 0 7.8 0 13.3-5.3 14.5-12.7 2.3-14.8 12.6-78.2 12.6-78.2s1.1-4.8-2-9.2zM52 11.2c2.1 0 3.8 1.7 3.8 3.8 0 .4-.1.8-.2 1.2l-7.3 2c.7-4 2.3-7 3.7-7zM36.1 18.2c2.2-2.9 5.5-4.8 9.3-5.3l3.5 19.3-10.8 3c.1-.4 1.3-12.5-2-17zm35.2 92.5c-.7 4.1-3.6 6.8-7.8 6.8H28.4c-2.2 0-4-1.5-4.7-3.8C22.6 110 12.7 73.8 11.4 64.1c-1.2-8.8-1.5-23-.8-26.1.7-2.8 3.1-3.6 3.1-3.6l10.9-3.1 15 82.2c.4 2.2 2.3 3.8 4.5 3.8 0 0 .1 0 .2 0 2.4-.2 4.1-2.2 4-4.6l-1.3-33.1 11.7-3.3 17 48.7z"/>
           </svg>
@@ -79,7 +79,7 @@ export const MarketplacesBar: React.FC = () => {
                 {mp.badge}
               </span>
 
-              <div className="my-5 h-12 flex items-center justify-center w-full">
+              <div className="my-5 h-12 flex items-center justify-center w-full overflow-visible">
                 <MarketplaceLogo id={mp.id} />
               </div>
 
